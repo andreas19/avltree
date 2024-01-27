8327dfdc5309776cca72444feac6818f07e203b7
andreas19 <andreas19@posteo.eu>
2024-01-27 13:28:51 +0100

    Replace constraints.Ordered with cmp.Ordered

--- avltree.go
+++ avltree.go
@@ -1,13 +1,13 @@
 // Package avltree implements a generic AVL tree.
 package avltree
 
-import "golang.org/x/exp/constraints"
+import "cmp"
 
 // A Cmp function compares two values and returns 0 if a == b, -1 if a < b, and 1 if a > b.
 type Cmp[T any] func(a, b T) int
 
 // CmpOrd is an implementation of the [Cmp] type for ordered values.
-func CmpOrd[T constraints.Ordered](a, b T) int {
+func CmpOrd[T cmp.Ordered](a, b T) int {
 	if a < b {
 		return -1
 	}
